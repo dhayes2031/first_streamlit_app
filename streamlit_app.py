d159521dcca9cad04a2502d13e26aeb448d35219
dhayes2031 <141437824+dhayes2031@users.noreply.github.com>
2023-08-04 11:28:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 
-streamlit.title('Breafast Menu')
+streamlit.header('Breafast Menu')
 
 
-streamlit.title('Omega 3 & Blueberry Oatmeal')
-streamlit.title('Kale, Spinach & Rocket Smoothie')
-streamlit.title('Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
