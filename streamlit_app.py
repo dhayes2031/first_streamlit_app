1fe26367252a6bcbb2333bbbe3246ddc4cd13c08
dhayes2031 <141437824+dhayes2031@users.noreply.github.com>
2023-08-28 12:14:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,3 +30,9 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 streamlit.text(fruityvice_response.json())
+
+#normalize json version
+fruityvice_normalized = pandas.json_normalized(fruityvice_response.json())
+#output to table
+streamlit.dataframe(fruityvice_normalized)
+
