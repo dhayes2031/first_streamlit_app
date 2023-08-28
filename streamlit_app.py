e2607760982d42e2ba97e53016e0002e17a4a76d
dhayes2031 <141437824+dhayes2031@users.noreply.github.com>
2023-08-28 12:27:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,6 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered', fruit_choice)
 
 import requests
-fruit_choice = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+streamlit.text(fruityvice_response.json())
 
