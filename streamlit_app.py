318fe020690a57f595a67facea1f1dc9e7463409
dhayes2031 <141437824+dhayes2031@users.noreply.github.com>
2023-08-28 12:52:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to dipslay fruitvice api resonse
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
 
