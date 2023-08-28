037f65799b27b618bc929eff1d714a42fd27c151
dhayes2031 <141437824+dhayes2031@users.noreply.github.com>
2023-08-28 12:48:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-streamlit.dataframe(fruit_choice)
+streamlit.text(fruit_choice.json())
+
 
 
